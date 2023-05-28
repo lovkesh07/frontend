5017abd054828d866c4a02f6c850d81376c37f74
Piyush <piyyush1219@gmail.com>
2023-05-28 15:52:38 +0530

    navdone

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <nav className="bg-white z-50 fixed top-0 left-0 w-full">
-      <div className="flex items-center font-medium justify-around">
-        <div className="z-50 p-5 w-full flex justify-between">
+      <div className="flex items-center font-medium justify-evenly">
+        <div className="z-50 p-5 w-fit flex">
           {/* <img src={Logo} alt="logo" className="md:cursor-pointer h-9" /> */}
           logo
-          <div className="text-3xl md:hidden " onClick={() => setOpen(!open)}>
-            <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
+          <div className="text-3xl md:hidden text-black " onClick={() => setOpen(!open)}>
+            <ion-icon name={`${open ? "close" : "menu"}`}/>
           </div>
         </div>
-        <ul className="md:flex hidden uppercase items-center gap-8 font-[Poppins]">
+        <ul className="md:flex hidden uppercase items-center gap-5 font-[Poppins]">
           <li>
             <Link to="/" className="py-7 px-3 inline-block">
               Home
